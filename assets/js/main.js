31b5edb0327939f77fd1246039bba2a54a2ed972
PhanTanPhuc <phuccode77@gmail.com>
2024-02-06 14:58:13 +0700

    login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 // add hovered class to selected list item 
-let list = document.querySelectorAll(".navigation li");
+// let list = document.querySelectorAll(".navigation li");
 
-function activelink() {
-    list.forEach(item => {
-        item.classList.remove("hovered");
-    });
-    this.classList.add("hovered");
-}
-list.forEach((item) => item.addEventListener("mouseover", activelink));
+// function activelink() {
+//     list.forEach(item => {
+//         item.classList.remove("hovered");
+//     });
+//     this.classList.add("hovered");
+// }
+// list.forEach((item) => item.addEventListener("mouseover", activelink));
 
 // Menu toggle
 let toggle = document.querySelector(".toggle");
